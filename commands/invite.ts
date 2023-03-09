7c3b5720bf8eab3fa8b5d72b2e77b81364263682
Sycraxe <sycraxe@gmail.com>
2023-03-09 19:02:34 +0100

    feat: add '/invite list' subcommand

--- commands/invite.ts
+++ commands/invite.ts
@@ -19,7 +19,11 @@ module.exports = {
                         .setDescription('Le nombre de secondes de validité du lien (0 pour l\'infini)')
                         .setMinValue(0)
                         .setMaxValue(604800)
-                        .setRequired(true))),
+                        .setRequired(true)))
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName('list')
+                .setDescription("Liste les invitations")),
 
     async execute(interaction) {
         const channel = interaction.options.getChannel('channel')
@@ -38,6 +42,24 @@ module.exports = {
                 ]
             })
 
+        } else if (interaction.options.getSubcommand() === 'list') {
+
+            const invites = await interaction.guild.invites.fetch().then()
+            let fields = []
+
+            for (const [key, value] of invites) {
+                fields.push({ name: 'https://discord.gg/' + key, value: 'par <@' + value.inviterId + '> \nvers <#' + value.channelId + '>' })
+            }
+
+            return interaction.reply({ embeds: [
+                new EmbedBuilder()
+                    .setColor('#16161d')
+                    .setTitle('Invite')
+                    .setDescription('Voici les différents liens actuellement valides sur le serveur')
+                    .setFields(fields)
+                    .setFooter({ text: `Pour plus d'informations concernant ${interaction.client.user.username}, exécutez la commande /help` })
+                ]
+            })
         }
     }
 }
