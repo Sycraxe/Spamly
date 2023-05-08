229fc0deda9a147a4c14bdbb815868821c767798
Sycraxe <sycraxe@gmail.com>
2023-05-08 22:29:36 +0200

    fix: prevent a too high role from being given

--- src/events/guildMemberAdd.ts
+++ src/events/guildMemberAdd.ts
@@ -7,6 +7,7 @@ module.exports = {
         member.guild.storage.data.autorole.roles.forEach((id) => {
             let role = member.guild.roles.cache.get(id)
             if (!role) return
+            if (role.comparePositionTo(member.guild.members.me.roles.highest) > 0) return
             member.roles.add(role)
         })
     },
